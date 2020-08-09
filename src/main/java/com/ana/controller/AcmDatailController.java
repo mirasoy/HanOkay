ce22e455e574bd41ae18e70b8c848d4adf18830d
Suhee Kim <62128361+dropTheBit-SH@users.noreply.github.com>
2020-08-09 18:29:41 +0900

    temp

--- src/main/java/com/ana/controller/AcmDatailController.java
+++ src/main/java/com/ana/controller/AcmDatailController.java
@@ -1,8 +1,13 @@
 package com.ana.controller;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +15,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ana.domain.AcmVO;
+import com.ana.domain.Criteria;
+import com.ana.domain.UserVO;
+import com.ana.domain.WishListVO;
 import com.ana.service.AcmDetailService;
 import com.ana.service.AcmService;
 import com.ana.service.CodeService;
 import com.ana.service.UserService;
+import com.ana.service.WishListService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -28,13 +38,14 @@ public class AcmDatailController { // 숙소 상세페이지
 	private AcmService acmService;
 	private CodeService codeService;
 	private UserService userService;
+	private WishListService wishservice;
 	
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
 	public void getDetailInfo(@RequestParam("acmNum") String acmNum, 
 			@RequestParam("in") String checkin,
 			@RequestParam("out") String checkout,
 			@RequestParam("person") String person,
-			Model model) {
+			Model model, HttpSession session) {
 		log.info("■■■■■■■■■■■■■■■■■■■■■■■■ 상세페이지로 이동 중...");
 		
 		if(person.isEmpty()) person = "1"; // 인원수가 없는 경우
@@ -47,9 +58,16 @@ public class AcmDatailController { // 숙소 상세페이지
 		.addAttribute("romCode", codeService.getRomCode())
 		.addAttribute("acmOwnerNum", userService.getAcmOwner(acmService.get(acmNum).getBizRegnum()).getUserNum());
 		String[] tmp = getDate(checkin , checkout);
+		
 		model.addAttribute("rom", service.getRomList(checkin,checkout,acmNum, person));
+		
 		model.addAttribute("in", tmp[0])
 		.addAttribute("out", tmp[1]);
+		/*
+		 * try { model.addAttribute("drawValue", add(acmNum, session)); } catch
+		 * (Exception e) { System.out.println("오류가 발생했습니다 : "+e.getMessage());
+		 * log.info("error@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"); return; }
+		 */
 	}
 	
 	// 경우의 수에 따른 체크인, 체크아웃 날짜 설정하기
@@ -107,5 +125,27 @@ public class AcmDatailController { // 숙소 상세페이지
 		cal2.add(Calendar.DATE, 1);
 		String[] rst = {sdf.format(cal1.getTime()), sdf.format(cal2.getTime())};
 		return rst;
+	}
+	
+	// 찜 버튼
+	public String add(String acmNum, HttpSession session){
+		
+		UserVO user= (UserVO)session.getAttribute("user"); 		
+		String userNum="";
+		
+		if(user != null){
+			userNum = user.getUserNum();
+		} 				
+		
+		WishListVO wish = new WishListVO();
+		wish.setAcmNum(acmNum);
+		wish.setUserNum(userNum);
+		
+		wishservice.register(wish);		
+		if(wishservice.register(wish) == 0) {			
+			return "fa fa-heart-o fa-2x openheart";	
+		}else {			
+			return "fa fa-heart fa-2x clsheart";	
+		}			
 	}
 }
